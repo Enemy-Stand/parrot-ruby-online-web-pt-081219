2e5e0c8183963232f1528473409b557b919f4ac8
Joseph Sesti <sestijoey63@gmail.com>
2019-08-13 18:40:27 +0000

    Automatically backed up by Learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -5,6 +5,8 @@ def parrot(phrase="Squawk!")
 end
 
 parrot
+
+parrot
 describe '#parrot' do
   it 'should output "Squawk!" to the terminal when called without any arguments' do
     expect($stdout).to receive(:puts).with("Squawk!")
